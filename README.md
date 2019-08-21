cf20e3e13ac5a19f1861c55083c3f6d821e795a7
Kris Luu <krisluu@Kriss-MacBook-Air.local>
2019-08-21 16:15:13 +0700

    update doji api...

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+## vAPI - API Tỉnh thành Việt Nam
+
+Retrieve a list of province
+```
+[GET] /api/province
+[GET] https://vapi.vnappmob.com/api/province
+```
+
+Retrieve a list of district in province_id
+```
+[GET] /api/province/{province_id}/
+[GET] /api/province/{province_id}/district
+[GET] https://vapi.vnappmob.com/api/province/{province_id}
+[GET] https://vapi.vnappmob.com/api/province/{province_id}/district
+```
+
+Retrieve a list of ward in district_id
+```
+[GET] /api/province/{province_id}/{district_id}/
+[GET] /api/province/{province_id}/district/{district_id}/
+[GET] /api/province/{province_id}/district/{district_id}/ward
+```
+
+---
 CREATE DATABASE vapi_db CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 mysql -u root -p vapi_db < vapi_db.sql;
 mysql -u root -p -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';"
--- app/api/gold.py
+++ app/api/gold.py
@@ -8,7 +8,7 @@ bp = Blueprint('api_gold', __name__)  # pylint: disable=C
 
 
 @bp.route('/api/gold/sjc', methods=['GET'])
-def api_province_get():
+def api_gold_sjc_get():
     """
     ```
     [GET] /api/gold/sjc?api_key={api_key}&date_from={date_from}&date_to={date_to}
@@ -47,7 +47,7 @@ def api_province_get():
 
 
 @bp.route('/api/gold/sjc', methods=['POST'])
-def api_vbiz_post():
+def api_gold_sjc_post():
     """
     New gold_sjc record
     ```
@@ -119,12 +119,124 @@ def api_vbiz_post():
 
                     data = (
                         '{"notification": {"title": "vPrice - Biến động giá SJC"'
-                        ',"body": "1L: Mua ' + '{:,d}'.format(int(json_data['buy_1l'])) + ' - Bán '
-                        + '{:,d}'.format(int(json_data['sell_1l'])) +
+                        ',"body": "1L: Mua ' + '{:,d}'.format(
+                            int(json_data['buy_1l'])) + ' - Bán ' +
+                        '{:,d}'.format(int(json_data['sell_1l'])) +
                         ' ","sound": "default"},"priority": "high",'
                         '"data": {"click_action": "FLUTTER_NOTIFICATION_CLICK",'
                         '"id": "1","status": "done"},'
                         '"to": "/topics/sjcgold"}')
+
+                    response = requests.post(
+                        'https://fcm.googleapis.com/fcm/send',
+                        headers=headers,
+                        data=data.encode('utf-8'))
+                    print(response)
+
+                    return make_response((jsonify({'results': 201})), 201)
+                return make_response((jsonify({'results': 200})), 200)
+            return error_response(403, 'Invalid api_key')
+        except MySQLdb.Error as err:  # pylint: disable=E
+            return error_response(400, str(err))
+        finally:
+            db_connect.close()
+    return error_response(404, str(db_connect.error))
+
+
+@bp.route('/api/gold/doji', methods=['GET'])
+def api_gold_doji_get():
+    """
+    ```
+    [GET] /api/gold/doji?api_key={api_key}&date_from={date_from}&date_to={date_to}
+    ```
+    """
+    db_connect = VDBConnect()
+    if db_connect.connected:
+        try:
+            request_api = request.args.get('api_key', default='', type=str)
+            date_from = request.args.get('date_from', default=0, type=int)
+            date_to = request.args.get('date_to', default=0, type=int)
+
+            slash_settings = db_connect.get_slash_setting()
+            if slash_settings['api'] == request_api:
+                statements = (
+                    "SELECT UNIX_TIMESTAMP(t1.datetime) as datetime, "
+                    "t1.buy_hcm, t1.sell_hcm, t1.buy_hn, t1.sell_hn "
+                    "FROM vnappmob_gold_doji t1 "
+                    "ORDER BY id DESC LIMIT 1;")
+                try:
+                    results = db_connect.readall(statements)
+                    return make_response((jsonify({'results': results})), 200)
+                except MySQLdb.Error as err:  # pylint: disable=E
+                    return error_response(400, str(err))
+            return error_response(403, 'Invalid api_key')
+        finally:
+            db_connect.close()
+    return error_response(404, str(db_connect.error))
+
+
+@bp.route('/api/gold/doji', methods=['POST'])
+def api_gold_doji_post():
+    """
+    New gold_doji record
+    ```
+    [POST] /api/gold/doji
+        -H 'Authorization: {api_key}'
+        -H 'Content-Type: application/json'
+        --data '{
+                    "buy_hcm": "{}",
+                    "sell_hcm": "{}"
+                }'
+    ```
+    """
+    db_connect = VDBConnect()
+    if db_connect.connected:
+        try:
+            request_api = request.headers['Authorization']
+            slash_settings = db_connect.get_slash_setting()
+            if slash_settings['api'] == request_api:
+                json_data = request.get_json()
+
+                last_row = db_connect.readone(
+                    "SELECT * FROM vnappmob_gold_doji "
+                    "ORDER BY id DESC LIMIT 1;")
+                if any([
+                        float(last_row['buy_hcm']) != float(
+                            json_data['buy_hcm']),
+                        float(last_row['sell_hcm']) != float(
+                            json_data['sell_hcm']),
+                        float(last_row['buy_hn']) != float(
+                            json_data['buy_hn']),
+                        float(last_row['sell_hn']) != float(
+                            json_data['sell_hn'])
+                ]):
+                    vals = []
+                    statements = ("INSERT INTO vnappmob_gold_doji "
+                                  "(id, buy_hcm, sell_hcm, buy_hn, sell_hn) "
+                                  "VALUES (NULL, %s, %s, %s, %s);")
+                    vals.extend((json_data['buy_hcm'], json_data['sell_hcm'],
+                                 json_data['buy_hn'], json_data['sell_hn']))
+                    db_connect.writecommit(statements, tuple(vals))
+
+                    headers = {
+                        'Authorization':
+                        'key=' + current_app.config['VPRICE_FCM_KEY'],
+                        'Content-Type':
+                        'application/json'
+                    }
+
+                    data = (
+                        '{"notification": {"title": "vPrice - Biến động giá DOJI"'
+                        ',"body": "HCM: Mua ' + '{:,d}'.format(
+                            int(json_data['buy_hcm'])) + ' - Bán ' +
+                        '{:,d}'.format(int(json_data['sell_hcm'])) +
+                        ' \nHN: Mua ' + '{:,d}'.format(
+                            int(json_data['buy_hn'])) + ' - Bán ' +
+                        '{:,d}'.format(int(json_data['sell_hn'])) +
+                        ' ","sound": "default"},"priority": "high",'
+                        '"data": {"click_action": "FLUTTER_NOTIFICATION_CLICK",'
+                        '"id": "1","status": "done"},'
+                        '"to": "/topics/dojigold"}')
 
                     response = requests.post(
                         'https://fcm.googleapis.com/fcm/send',
